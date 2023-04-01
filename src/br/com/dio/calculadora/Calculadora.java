212360b43781f6658138e9eba1ca20b844a35abc
henriqueesm <henriquefera.hsm@gmail.com>
2023-04-01 18:49:28 -0300

    Alterando tipo de retorno

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -18,7 +18,7 @@ public class Calculadora {
 		int soma = soma(a, b);
 		int subtracao = subtracao(a, b);
 		int multiplicacao = multiplicacao(a,b);
-		int divisao = divisao(a, b);
+		double divisao = divisao(a, b);
 		
 		System.out.println(soma);
 		System.out.println(subtracao);
@@ -36,7 +36,7 @@ public class Calculadora {
 		return a - b;
 	}
 
-	public static int divisao(int a, int b) {
+	public static double divisao(double a, int b) {
 		return a / b;
 	}
 
